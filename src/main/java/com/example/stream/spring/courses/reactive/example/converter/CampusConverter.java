14a6352487c531972c428f7f7b542d5cf689309b
U479687 <fabian.aspeeencina@intesasanpaolo.com>
2025-03-27 16:15:38 +0100

    create campus converter

--- src/main/java/com/example/stream/spring/courses/reactive/example/converter/CampusConverter.java
+++ src/main/java/com/example/stream/spring/courses/reactive/example/converter/CampusConverter.java
@@ -2,8 +2,25 @@ package com.example.stream.spring.courses.reactive.example.converter;
 
 import com.example.stream.spring.courses.reactive.example.entity.Campus;
 import com.example.stream.spring.courses.reactive.example.model.request.CampusRequestDto;
+import com.example.stream.spring.courses.reactive.example.model.response.CampusResponseDto;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CampusConverter implements Converter<CampusRequestDto, Campus> {
+public class CampusConverter implements Converter<CampusRequestDto, CampusResponseDto, Campus> {
+    @Override
+    public CampusResponseDto toDto(Campus entity) {
+        return new CampusResponseDto(entity.getName(), entity.getUniversityId(), entity.getCountry(), entity.getCity(),
+                entity.getStartDate(), entity.getEndDate(), entity.getCreatedAt(), entity.getUpdatedAt(), entity.getName());
+    }
+
+    @Override
+    public Campus toEntity(CampusRequestDto dto) {
+        Campus entity = new Campus();
+        entity.setAddress(dto.address());
+        entity.setUniversityId(dto.universityId());
+        entity.setCountry(dto.country());
+        entity.setCity(dto.city());
+        //TODO: understand when is update and when is create
+        return null;
+    }
 }
