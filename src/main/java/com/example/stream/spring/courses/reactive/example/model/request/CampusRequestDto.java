2a0bdfede197eab0e172d56243fcb2dbd8b41109
fabianaspeeencina <faspeeencina@gmail.com>
2025-04-13 10:42:04 +0200

    added request dto for entities

--- src/main/java/com/example/stream/spring/courses/reactive/example/model/request/CampusRequestDto.java
+++ src/main/java/com/example/stream/spring/courses/reactive/example/model/request/CampusRequestDto.java
@@ -3,6 +3,5 @@ package com.example.stream.spring.courses.reactive.example.model.request;
 import java.time.LocalDateTime;
 
 public record CampusRequestDto(String name, String address, Long universityId, String country, String city,
-                               LocalDateTime startDate, LocalDateTime endDate,
-                               String identifier) {
+                               LocalDateTime startDate, LocalDateTime endDate, String identifier) {
 }
--- src/main/java/com/example/stream/spring/courses/reactive/example/model/response/InstructorResponseDto.java
+++ src/main/java/com/example/stream/spring/courses/reactive/example/model/response/InstructorResponseDto.java
@@ -1,4 +1,7 @@
 package com.example.stream.spring.courses.reactive.example.model.response;
 
-public record InstructorResponseDto() {
+import java.time.LocalDateTime;
+
+public record InstructorResponseDto(String instructorId, String name, String email, LocalDateTime createdAt,
+                                    LocalDateTime updatedAt) {
 }
