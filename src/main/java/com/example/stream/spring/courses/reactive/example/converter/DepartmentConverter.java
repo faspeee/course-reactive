743ac58b5ad07519a5edc051afc79e3eb30cb8b5
fabianaspeeencina <faspeeencina@gmail.com>
2025-04-15 22:26:38 +0200

    added service to department entity

--- src/main/java/com/example/stream/spring/courses/reactive/example/converter/DepartmentConverter.java
+++ src/main/java/com/example/stream/spring/courses/reactive/example/converter/DepartmentConverter.java
@@ -1,7 +1,19 @@
 package com.example.stream.spring.courses.reactive.example.converter;
 
+import com.example.stream.spring.courses.reactive.example.entity.Department;
+import com.example.stream.spring.courses.reactive.example.model.request.DepartmentRequestDto;
+import com.example.stream.spring.courses.reactive.example.model.response.DepartmentResponseDto;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DepartmentConverter {
+public class DepartmentConverter implements Converter<DepartmentRequestDto, DepartmentResponseDto, Department> {
+    @Override
+    public DepartmentResponseDto toDto(Department entity) {
+        return null;
+    }
+
+    @Override
+    public Department toEntity(DepartmentRequestDto dto) {
+        return null;
+    }
 }
--- src/main/java/com/example/stream/spring/courses/reactive/example/service/DepartmentService.java
+++ src/main/java/com/example/stream/spring/courses/reactive/example/service/DepartmentService.java
@@ -1,4 +1,59 @@
 package com.example.stream.spring.courses.reactive.example.service;
 
+import com.example.stream.spring.courses.reactive.example.converter.DepartmentConverter;
+import com.example.stream.spring.courses.reactive.example.entity.Department;
+import com.example.stream.spring.courses.reactive.example.model.request.DepartmentRequestDto;
+import com.example.stream.spring.courses.reactive.example.model.response.DepartmentResponseDto;
+import com.example.stream.spring.courses.reactive.example.repository.DepartmentRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Service
 public class DepartmentService {
+    private final DepartmentRepository departmentRepository;
+    private final DepartmentConverter departmentConverter;
+
+    public DepartmentService(DepartmentRepository departmentRepository, DepartmentConverter departmentConverter) {
+        this.departmentRepository = departmentRepository;
+        this.departmentConverter = departmentConverter;
+    }
+
+    public Flux<DepartmentResponseDto> getAllDepartments() {
+        return departmentRepository.findAll()
+                .map(departmentConverter::toDto);
+    }
+
+    public Mono<DepartmentResponseDto> createDepartment(DepartmentRequestDto departmentRequestDto) {
+        return departmentRepository.save(departmentConverter.toEntity(departmentRequestDto))
+                .map(departmentConverter::toDto);
+    }
+
+    public Mono<DepartmentResponseDto> getDepartmentById(String departmentId) {
+        return getDepartmentById(UUID.fromString(departmentId))
+                .map(departmentConverter::toDto);
+    }
+
+    private Department updateDepartment(Department department, DepartmentRequestDto departmentRequestDto) {
+    }
+
+    private Mono<Department> getDepartmentById(UUID departmentId) {
+        return departmentRepository.findById(departmentId);
+    }
+
+    public Mono<DepartmentResponseDto> updateDepartment(String departmentId, DepartmentRequestDto departmentRequestDto) {
+        return getDepartmentById(UUID.fromString(departmentId))
+                .map(department -> updateDepartment(department, departmentRequestDto))
+                .flatMap(department -> departmentRepository.save(department)
+                        .map(departmentConverter::toDto))
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "department not found")));
+    }
+
+    public Mono<Void> deleteDepartment(String departmentId) {
+        return departmentRepository.deleteById(UUID.fromString(departmentId));
+    }
 }
